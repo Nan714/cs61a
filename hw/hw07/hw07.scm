1195942074df258645d4db4118f7209b04b6ce56
Nan <nanw70104@gmail.com>
2026-07-28 15:25:36 +0800

    hw07_scheme

--- hw/hw07/hw07.scm
+++ hw/hw07/hw07.scm
@@ -1,15 +1,19 @@
 (define (square n) (* n n))
 
-(define (pow base exp) ())
+(define (pow base exp) 
+    (cond ((= exp 0) 1)
+           ((even? exp) (pow (square base) (/ exp 2)))
+           (else (* base(pow base (- exp 1) )))))
 
 (define (repeatedly-cube n x)
   (if (zero? n)
       x
-      (let (_________________)
+      (let ((y (repeatedly-cube (- n 1) x)))
         (* y y y))))
+
 
 (define (cddr s) (cdr (cdr s)))
 
-(define (cadr s) 'YOUR-CODE-HERE)
+(define (cadr s) (car (cdr s)))
 
-(define (caddr s) 'YOUR-CODE-HERE)
+(define (caddr s) (car (cdr (cdr s))))
